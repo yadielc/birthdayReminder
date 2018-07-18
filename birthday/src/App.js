b61f29fdfdfe1cb5fa3e83498b59f7199f312198
Yadiel <ycabr035@fiu.edu>
2018-07-18 15:01:51 -0400

    added react-mdl

--- birthday/src/App.js
+++ birthday/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { } from 'react-mdl';
+import {Layout, Navigation, Drawer, Content } from 'react-mdl';
 
 class App extends Component {
   render() {
