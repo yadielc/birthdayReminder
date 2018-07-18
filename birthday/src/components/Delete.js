f4e4f2422f501ac59db4e63a9fe2b4afa485f647
Yadiel <ycabr035@fiu.edu>
2018-07-18 15:22:15 -0400

    fixed errors

---
+++ birthday/src/components/Delete.js
@@ -0,0 +1,13 @@
+import React, {Component} from ' react';
+
+class Add extends Component {
+  render() {
+    return(
+
+      <div><h1> Delete Birthdays Page</h1></div>
+    )
+  }
+
+}
+
+export default Delete;
