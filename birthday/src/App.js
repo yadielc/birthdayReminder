bcad730935a56ea4e011ff2a741fffe74a490b72
Yadiel <ycabr035@fiu.edu>
2018-07-18 15:01:15 -0400

    added react-mdl

--- birthday/src/App.js
+++ birthday/src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { } from 'react-mdl';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+  <div className="demo-big-content">
+      <Layout>
+          <Header title="Title" scroll>
+              <Navigation>
+                  <a href="#">Link</a>
+                  <a href="#">Link</a>
+                  <a href="#">Link</a>
+                  <a href="#">Link</a>
+              </Navigation>
+          </Header>
+          <Drawer title="Title">
+              <Navigation>
+                  <a href="#">Link</a>
+                  <a href="#">Link</a>
+                  <a href="#">Link</a>
+                  <a href="#">Link</a>
+              </Navigation>
+          </Drawer>
+          <Content>
+              <div className="page-content" />
+          </Content>
+      </Layout>
+  </div>
     );
   }
 }
