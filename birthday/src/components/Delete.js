b846a681c8ba079296456163a6e3f30b4fc9b5c3
Yadiel <ycabr035@fiu.edu>
2018-07-18 15:45:05 -0400

    added routes

--- birthday/src/components/Delete.js
+++ birthday/src/components/Delete.js
@@ -1,6 +1,6 @@
-import React, {Component} from ' react';
+import React, {Component} from 'react';
 
-class Add extends Component {
+class Delete extends Component {
   render() {
     return(
 
--- birthday/src/components/Main.js
+++ birthday/src/components/Main.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import LandingPage from './LandingPage';
+import Add from './Add';
+import Delete from './Delete';
 import {Switch, Route} from 'react-router-dom';
 
 const Main = () => (
