3ea23c3976d2dcf3b066ac6b6d22b622caa8a065
Yadiel <ycabr035@fiu.edu>
2018-07-18 15:32:41 -0400

    added landing page component

--- birthday/src/components/Main.js
+++ birthday/src/components/Main.js
@@ -1,10 +1,11 @@
 /* This component is going to have all the routes */
 
 import React from 'react';
+import LandingPage from './LandingPage';
 
 const Main = () => {
  <Switch>
-  <Router exact path="/" component={LandingPage}
+  <Router exact path="/" component={LandingPage} />
  </Switch>
 
 }
