eac7ef858e8ec410c97edca5657758584801b910
Yadiel <ycabr035@fiu.edu>
2018-08-03 14:39:06 -0400

    added upcoming birthdays component

--- birthday/src/components/UpComing.js
+++ birthday/src/components/UpComing.js
@@ -1,21 +1,17 @@
 import React, {Component} from 'react';
 import Panel from 'react-bootstrap/lib/Panel'
 
-class Add extends Component {
+class UpComing extends Component {
   render() {
     return(
 
-      <div><h1> Add Birthdays Page</h1>
 
-      <Panel>
-      Upcoming Birthdays:<br>
-      Here all the upcoming birthdays go.<br> 
-      </Panel>
 
-      </div>
+    
+
     )
   }
 
 }
 
-export default Add;
+export default UpComing;
