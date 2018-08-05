7f22828afc95be296e0fdc83f7e8234664c5c63e
Yadiel <ycabr035@fiu.edu>
2018-08-04 21:53:24 -0400

    fixed all the errors on the add component

--- birthday/src/components/Add.js
+++ birthday/src/components/Add.js
@@ -3,14 +3,25 @@ import React, {Component} from 'react';
 class Add extends Component {
 
   render() {
+      let birthdays = this.state.birthdays;
     return(
 
+      <div>
       <form onSubmit={this.handleCreate.bind(this)}>
                 <input type="text" placeholder="Name" ref="nameForm" />
                 <button onClick ={(e)=>this.formSubmit(e)}> Add</button>
-
-
             </form>
+            <pre>
+            {birthdays.map((birthday,i) =>
+              <li key={i}>
+              {i+1}, {birthday.name}, {birthday.date}
+              <button onClick={()=>this.fRemove(i)}>Remove</button>
+              <button onClick={()=>this.fEdit(i)}>Edit</button>
+              </li>
+
+            )}
+            </pre>
+            </div>
     )
   }
 
