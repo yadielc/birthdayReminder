587cdd3f71b29127982aed96660f4fa27015741b
Yadiel <ycabr035@fiu.edu>
2018-08-04 21:02:41 -0400

    removed unused libraries

--- birthday/src/App.js
+++ birthday/src/App.js
@@ -8,7 +8,6 @@ import Panel from 'react-bootstrap/lib/Panel'
 import Button from 'react-bootstrap/lib/Button'
 import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar'
 import Modal from 'react-bootstrap/lib/Modal'
-import FormGroup from 'react-bootstrap/lib/FormGroup'
 import ControlLabel from 'react-bootstrap/lib/ControlLabel'
 import FormControl from 'react-bootstrap/lib/FormControl'
 import UpComing from './components/UpComing';
