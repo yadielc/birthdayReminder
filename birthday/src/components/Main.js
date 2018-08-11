63c1956e303c9c60213812446f92bd8b119ba563
Yadiel <ycabr035@fiu.edu>
2018-08-10 20:56:29 -0400

    changed routing structure, deleted add component

--- birthday/src/components/Main.js
+++ birthday/src/components/Main.js
@@ -2,14 +2,12 @@
 
 import React from 'react';
 import LandingPage from './LandingPage';
-import Add from './Add';
 import Delete from './Delete';
 import {Switch, Route} from 'react-router-dom';
 
 const Main = () => (
  <Switch>
   <Route exact path="/" component={LandingPage} />
-  <Route path="/Add" component={Add}/>
     <Route path="/Delete" component={Delete}/>
 
  </Switch>
