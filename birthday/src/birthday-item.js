dbec6d97c4e8caae819d7a4fd016d58373e7bf0c
Yadiel <ycabr035@fiu.edu>
2018-08-10 21:59:23 -0400

    added new file

--- birthday/src/birthday-item.js
+++ birthday/src/birthday-item.js
@@ -16,44 +16,13 @@ import Add from './components/Add';
 
 
 class App extends Component {
-  
+
 render() {
     return (
-  <div className="demo-big-content">
-      <Layout>
-          <Header title="Birthday Reminder Application" scroll>
-              <Navigation>
-                  <Link to="/LandingPage">Home</Link>
-                      <Link to="/Delete">Delete</Link>
-              </Navigation>
-          </Header>
-
-          <Content>
-              <div className="page-content" />
-              <h1>HELLO</h1>
-
-              <div>
-                <Panel>
-                  Upcoming Birthdays:<br/>
-                  // Added upcoming birthdays using the current state of the array
-                  {
-                    this.state.birthdays.map(birthday=> {
-                       return (
-                         <div key={birthday.name}>
-                         <span>{birthday.name}</span> | <span>{birthday.date}</span>
-                        <button>Delete</button>
-                        </div>
-                       );
-
-                    })
-                  }
-                </Panel>
-              </div>
-
-              <Main/>
-          </Content>
-      </Layout>
-  </div>
+  <div>
+    <span>{birthday.name}</span> | <span>{birthday.date}</span>
+    <button>Delete</button>
+    </div>
     );
   }
 
